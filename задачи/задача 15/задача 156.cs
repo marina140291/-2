f7c68780ae0349411c2afb81c5c7015815966d1f
marina140291 <marina140291@mail.ru>
2022-12-21 23:35:20 +0300

    Задача 15

---
+++ задачи/задача 15/задача 156.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a < 1 || a > 7) {
+    Console.WriteLine("Вы ошиблись !! Введите число");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+if (a==1) Console.WriteLine("Нет");
+if (a==2) Console.WriteLine("Нет");
+if (a==3) Console.WriteLine("Нет");
+if (a==4) Console.WriteLine("Нет");
+if (a==5) Console.WriteLine("Нет");
+if (a==6) Console.WriteLine("ДА");
+if (a==7) Console.WriteLine("ДА");   
+
+
+
+
